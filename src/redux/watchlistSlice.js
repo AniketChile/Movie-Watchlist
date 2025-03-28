94b9ea97974c2436798109a2a6cdb64c20b203cf
AniketChile <84616134+AniketChile@users.noreply.github.com>
2025-03-28 05:59:56 +0530

    store and slice configured

---
+++ src/redux/watchlistSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    movies: JSON.parse(localStorage.getItem("watchlist")) || []
+}
+
+const watchlistSlice = createSlice({
+    name: "watchlist",
+    initialState,
+    reducers:{
+        addToWatchlist:(state,action)=>{
+           const movieExists = state.movies.some((movie)=>movie.imdbID === action.payload.imdbID);
+           if(!movieExists){
+            state.movies.push(action.payload);
+            localStorage.setItem("watchlist",JSON.stringify(state.movies));
+           }
+        },
+        removeFromWatchlist:(state,action)=>{
+            state.movies = state.movies.filter((movie)=>movie.imdbID !== action.payload)
+            localStorage.setItem("watchlist", JSON.stringify(state.movies));
+        }
+
+    }
+})
+
+export const {addToWatchlist,removeFromWatchlist} = watchlistSlice.actions;
+export default watchlistSlice.reducer
